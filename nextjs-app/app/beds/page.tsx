a5ff9bfc5418bfc854dd8213eaa992689b368a40
robert <robban2005@gmail.com>
2025-06-15 18:40:14 +0700

    layout

--- nextjs-app/app/beds/page.tsx
+++ nextjs-app/app/beds/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { createClient } from "@/lib/supabase/server";
 import { redirect } from "next/navigation";
+import { PageLayout } from "@/components/page-layout";
 
 export default async function Page() {
   const supabase = await createClient();
@@ -12,5 +13,9 @@ export default async function Page() {
 
   const beds = await prisma.bed.findMany()
 
-  return <pre>{JSON.stringify(beds, null, 2)}</pre>
+  return (
+    <PageLayout>
+      <pre>{JSON.stringify(beds, null, 2)}</pre>
+    </PageLayout>
+  );
 }
